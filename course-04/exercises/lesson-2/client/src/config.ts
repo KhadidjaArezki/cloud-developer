4866d0ac1b19b340b59457148a3d52bb689def10
Khadidja Arezki <khadidjaarezki999@gmail.com>
2023-02-28 18:14:07 +0100

    Finish CDND Final Project

--- course-04/exercises/lesson-2/client/src/config.ts
+++ course-04/exercises/lesson-2/client/src/config.ts
@@ -1,1 +1,1 @@
-export const apiEndpoint = '...'
+export const apiEndpoint = 'https://r0bh0mb84g.execute-api.us-east-1.amazonaws.com/dev'
--- course-04/exercises/lesson-2/starter-code/index.js
+++ course-04/exercises/lesson-2/starter-code/index.js
@@ -8,22 +8,27 @@ const groupsTable = process.env.GROUPS_TABLE
 
 exports.handler = async (event) => {
   console.log('Processing event: ', event)
-
-  // TODO: Read and parse "limit" and "nextKey" parameters from query parameters
-  // let nextKey // Next key to continue scan operation if necessary
-  // let limit // Maximum number of elements to return
-
+  
   // HINT: You might find the following method useful to get an incoming parameter value
-  // getQueryParameter(event, 'param')
-
-  // TODO: Return 400 error if parameters are invalid
+  // DONE: Read and parse "limit" and "nextKey" parameters from query parameters
+  
+  let nextKeyStr = getQueryParameter(event, 'nextKey') // Next key to continue scan operation if necessary
+  let nextKey =  JSON.parse(decodeURIComponent(nextKeySrt))
+  
+  let limitStr = getQueryParameter(event, 'limit') // Maximum number of elements to return
+  let limit = parseInt(limitStr)
+  
+  // DONE: Return 400 error if parameters are invalid
+  if ((!limit || limit < 0) || !nextKey) return {
+    statusCode: 400,
+  }
 
   // Scan operation parameters
   const scanParams = {
     TableName: groupsTable,
-    // TODO: Set correct pagination parameters
-    // Limit: ???,
-    // ExclusiveStartKey: ???
+    // DONE: Set correct pagination parameters
+    Limit: limit,
+    ExclusiveStartKey: nextKey
   }
   console.log('Scan params: ', scanParams)
 
